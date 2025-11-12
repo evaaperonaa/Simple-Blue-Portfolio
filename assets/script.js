8fcb9638336bfa323eab2456ceb1092f605bda42
Evly <evaaperonaa@gmail.com>
2025-11-12 17:44:02 +0100

    Contact Added and Functional

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const socialsSection = document.getElementById('socials-section');
+const emailSection = document.getElementById('email-section');
+const toggleToEmail = document.getElementById('toggle-to-email');
+const toggleToSocials = document.getElementById('toggle-to-socials');
+
+// Función para mostrar la sección de email (formulario)
+toggleToEmail.addEventListener('click', () => {
+    socialsSection.classList.remove('active');
+    emailSection.classList.add('active');
+});
+
+// Función para mostrar la sección de socials
+toggleToSocials.addEventListener('click', () => {
+    emailSection.classList.remove('active');
+    socialsSection.classList.add('active');
+});
